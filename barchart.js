7a7af6e98652806a5f37b2dfb74d39fb64e72cdc
huangyvonnee <huangyvonnee@gmail.com>
2018-04-30 06:53:37 -0500

    tinkering with d3..

--- barchart.js
+++ barchart.js
@@ -99,13 +99,8 @@ svg.selectAll(".text")
            .attr("y", function(d) { return y(d.cuisine) - 20; })
            .attr("dy", ".35em")
            .attr("transform", "rotate(90)")
-           .text(function(d) { return d.cuisine; });      
+           .text(function(d) { return d.cuisine; })
+           .style("text-anchor", "start");      
 var x = d3.scaleBand().rangeRound([0,915])
 x.domain(data.map(function(d){return d.cuisine;}))
-svg.append("g")
-    .attr("transform", "translate(0, 415)")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", function(d) {
-        return "rotate(90)" 
-        });
+
